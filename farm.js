929fcfc7fae2d8f41510c8991b1fd3ab4145fb3c
BS-Summit <98066525+BS-Summit@users.noreply.github.com>
2022-01-19 17:42:58 -0800

    Create farm.js

---
+++ farm.js
@@ -0,0 +1,27 @@
+/** @param {NS} ns **/
+/*
+ * args[0:n] - Names of target servers
+ */
+export async function main(ns, args) {
+	await ns.killall;
+	let targets = ns.args
+	let hackScript = "hack.js";
+	let scriptRam = ns.getScriptRam(hackScript);
+	let serverRam = ns.getServerMaxRam("home");
+	let maxThreads = Math.floor((serverRam - 8) / scriptRam);
+
+	for (let i = targets.length - 1; i >= 0; i--) {
+		let target = ns.args[i]
+		let targetSecurity = ns.getServerMinSecurityLevel(target) + 2;
+		let targetMoney = ns.getServerMaxMoney(target) * 0.9;
+		let threads = Math.floor(maxThreads / targets.length);
+		let paydayCalc = Math.round(targetMoney / threads);
+		let payday = paydayCalc.toString().length;
+		ns.tprint(`Home is hacking ${target} with ${threads} threads. Payday is ~${payday}.`);
+		if (i == 0) {
+			ns.spawn(hackScript, threads, target, threads, targetSecurity, targetMoney);
+		} else {
+			ns.run(hackScript, threads, target, threads, targetSecurity, targetMoney);
+		}
+	}
+}
