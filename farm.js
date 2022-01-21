5e45437b293e407d1c48816875fd09c9de1335cd
BS-Summit <98066525+BS-Summit@users.noreply.github.com>
2022-01-21 08:25:08 -0800

    Updated farm & improved display.
    Increased sophistication of the reserved ram for other scripts, by calculating the needed ram, and rounding out the last target's threads to make sure the reserved ram is available, and no extra threads are missed due to rounding.

--- farm.js
+++ farm.js
@@ -1,27 +1,61 @@
 /** @param {NS} ns **/
 /*
- * args[0:n] - Names of target servers
+ * args[0-n] - Names of target servers
  */
 export async function main(ns, args) {
 	await ns.killall;
-	let targets = ns.args
-	let hackScript = "hack.js";
-	let scriptRam = ns.getScriptRam(hackScript);
-	let serverRam = ns.getServerMaxRam("home");
-	let maxThreads = Math.floor((serverRam - 8) / scriptRam);
+	var targetList = ns.args;
+	var targets = targetList.join(" ");
+	var reservedRam = 0;
 
-	for (let i = targets.length - 1; i >= 0; i--) {
-		let target = ns.args[i]
+	ns.tprint(`======================================================================`);
+	var serverRam = ns.getServerMaxRam("home");
+	ns.tprint(`Total Available RAM: ${serverRam} GB`)
+	var updateScript = "update.js"
+	var updateRam = ns.getScriptRam(updateScript);
+	ns.tprint(` Update Script RAM: ${updateRam} GB`)
+	var hackScript = "hack.js";
+	var scriptRam = ns.getScriptRam(hackScript);
+	ns.tprint(` Hack Script RAM: ${scriptRam} GB`)
+	var maxThreads = Math.floor(serverRam / scriptRam);
+	var remainingThreads = maxThreads;
+	ns.tprint(`Max Threads: ${maxThreads}`)
+	var threads = Math.floor(maxThreads / targetList.length);
+	ns.tprint(` Threads: ${threads} / target`)
+	ns.tprint(`======================================================================`);
+	ns.tprint(`Farm List:  ${targets}`)
+	ns.tprint(`----------------------------------------------------------------------`);
+	for (let i = targetList.length - 1; i >= 0; i--) {
+		let target = ns.args[i];
 		let targetSecurity = ns.getServerMinSecurityLevel(target) + 2;
 		let targetMoney = ns.getServerMaxMoney(target) * 0.9;
-		let threads = Math.floor(maxThreads / targets.length);
-		let paydayCalc = Math.round(targetMoney / threads);
-		let payday = paydayCalc.toString().length;
-		ns.tprint(`Home is hacking ${target} with ${threads} threads. Payday is ~${payday}.`);
+		let payday = Math.floor(Math.log(targetMoney / threads) * 100) / 100;
+		let textBufferTarget = " ".repeat(16 - target.length);
+		let textBufferThread = " ".repeat(6 - threads.toString().length);
 		if (i == 0) {
+			reservedRam = serverRam - ((maxThreads - remainingThreads + threads) * scriptRam);
+			threads = remainingThreads
+
+			while (reservedRam >= (updateRam + scriptRam)) { threads++; reservedRam = reservedRam - scriptRam; }
+			while (reservedRam < updateRam) { threads--; reservedRam = reservedRam + scriptRam; }
+
+			payday = Math.floor(Math.log(targetMoney / threads) * 100) / 100;
+			let reservedramReadable = Math.floor(reservedRam * 100) / 100;
+			textBufferThread = " ".repeat(6 - threads.toString().length);
+			ns.tprint(`  Home             --->${textBufferThread} ${threads} threads  --->  ${target}:${textBufferTarget} \$${payday.toFixed(2)}`);
+			ns.tprint(`======================================================================`);
+			ns.tprint(`${reservedramReadable}GB reserved for scripts.`);
+
 			ns.spawn(hackScript, threads, target, threads, targetSecurity, targetMoney);
 		} else {
+			ns.tprint(`  Home             --->${textBufferThread} ${threads} threads  --->  ${target}:${textBufferTarget} \$${payday.toFixed(2)}`);
 			ns.run(hackScript, threads, target, threads, targetSecurity, targetMoney);
+			remainingThreads = remainingThreads - threads;
 		}
 	}
 }
+/*
+function payday (ns,targetMoney,threads) {
+	return Math.floor(Math.log(targetMoney / threads) * 100) / 100;
+}
+*/
