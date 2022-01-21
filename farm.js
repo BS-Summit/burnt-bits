7da69b479aa02433024a94c9647c2527e466e832
BS-Summit <98066525+BS-Summit@users.noreply.github.com>
2022-01-21 11:11:05 -0800

    Update farm.js
    Needed to increase the size and buffer values, to make sure there isn't overflow.

--- farm.js
+++ farm.js
@@ -8,7 +8,7 @@ export async function main(ns, args) {
 	var targets = targetList.join(" ");
 	var reservedRam = 0;
 
-	ns.tprint(`======================================================================`);
+	ns.tprint(`============================================================================`);
 	var serverRam = ns.getServerMaxRam("home");
 	ns.tprint(`Total Available RAM: ${serverRam} GB`)
 	var updateScript = "update.js"
@@ -22,16 +22,16 @@ export async function main(ns, args) {
 	ns.tprint(`Max Threads: ${maxThreads}`)
 	var threads = Math.floor(maxThreads / targetList.length);
 	ns.tprint(` Threads: ${threads} / target`)
-	ns.tprint(`======================================================================`);
+	ns.tprint(`============================================================================`);
 	ns.tprint(`Farm List:  ${targets}`)
-	ns.tprint(`----------------------------------------------------------------------`);
+	ns.tprint(`----------------------------------------------------------------------------`);
 	for (let i = targetList.length - 1; i >= 0; i--) {
 		let target = ns.args[i];
 		let targetSecurity = ns.getServerMinSecurityLevel(target) + 2;
 		let targetMoney = ns.getServerMaxMoney(target) * 0.9;
 		let payday = Math.floor(Math.log(targetMoney / threads) * 100) / 100;
-		let textBufferTarget = " ".repeat(16 - target.length);
-		let textBufferThread = " ".repeat(6 - threads.toString().length);
+		let textBufferTarget = " ".repeat(18 - target.length);
+		let textBufferThread = " ".repeat(8 - threads.toString().length);
 		if (i == 0) {
 			reservedRam = serverRam - ((maxThreads - remainingThreads + threads) * scriptRam);
 			threads = remainingThreads
@@ -41,9 +41,9 @@ export async function main(ns, args) {
 
 			payday = Math.floor(Math.log(targetMoney / threads) * 100) / 100;
 			let reservedramReadable = Math.floor(reservedRam * 100) / 100;
-			textBufferThread = " ".repeat(6 - threads.toString().length);
+			textBufferThread = " ".repeat(8 - threads.toString().length);
 			ns.tprint(`  Home             --->${textBufferThread} ${threads} threads  --->  ${target}:${textBufferTarget} \$${payday.toFixed(2)}`);
-			ns.tprint(`======================================================================`);
+			ns.tprint(`============================================================================`);
 			ns.tprint(`${reservedramReadable}GB reserved for scripts.`);
 
 			ns.spawn(hackScript, threads, target, threads, targetSecurity, targetMoney);
