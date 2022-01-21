f90a030d5eb8eec5521aaaf9546c23741a5cd86f
BS-Summit <98066525+BS-Summit@users.noreply.github.com>
2022-01-21 08:35:21 -0800

    New test.js
    Replaced previous test.js which was unnecessary, `analyze` replicated the same function.
    New test.js is not yet functional, but is intended to allow you to run script commands via terminal.

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 /** @param {NS} ns **/
 export async function main(ns, args) {
-	let target = ns.args[0];
-	let currentMoney = ns.getServerMoneyAvailable(target);
-	let maxMoney = ns.getServerMaxMoney(target);
-	ns.tprint(`${target} has \$${currentMoney} of \$${maxMoney} available.`);
+	let command = ns.args[0];
+	let parameterList = ns.args.slice(1);
+	let threadCheck = parameterList.findIndex(parameter => parameter == "-t");
+	let threads = 1;
+	if (threadCheck > -1) {
+		threads = parameterList[threadCheck + 1];
+		parametersList = ns.args.splice(threadCheck, 2);
+	}
+	let parameters = parameterList.join(" ");
+	ns.tprint(`Executing ${command} with ${threads} threads.`);
+	if (parameterList.length > 0) { ns.tprint(`Including arguments: ${parameters}`); }
+	ns.run(command, threads, parameters);
+	//ns.tprint(ns.run(command, threads, parameters));
 }
